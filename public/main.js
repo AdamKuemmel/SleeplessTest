e2b6d0debcdd2027c1ae13e28636e205a1e81df9
AdamKuemmel <Adamkuemmel@gmail.com>
2021-10-20 11:19:30 -0600

    first push

---
+++ public/main.js
@@ -0,0 +1,21 @@
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.6 + "px";
+  console.log(parallax);
+});
+
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+window.addEventListener("scroll", function () {
+  let hideAK = document.querySelector("article");
+  hideAK.classList.toggle("hiddenAK", window.scrollY > 0);
+});
+window.addEventListener("scroll", function () {
+  let hideAK = document.getElementsById("navSleep");
+  hideAK.classList.toggle("hiddenAK", window.scrollY > 0);
+});
